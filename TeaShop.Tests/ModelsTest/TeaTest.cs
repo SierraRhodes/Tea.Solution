667bd23de7d8473862218c5c98914e0de50a1f05
SierraRhodes <alreadywrote@gmail.com>
2023-04-19 19:19:39 -0700

    add set name test passed

--- TeaShop.Tests/ModelsTest/TeaTest.cs
+++ TeaShop.Tests/ModelsTest/TeaTest.cs
@@ -12,5 +12,20 @@ namespace Teas.Tests
     {
       Tea testTea  = new Tea { Name = "Green Tea", Price = 2.99m };
     }
+    [TestMethod]
+    public void SetName_ValidName_SetName()
+    {
+    //Arrange 
+    string expectedName = "Jasmine Tea";
+    decimal expectedPrice = 2.99m;
+    Tea testTea = new Tea { Name = "Green Tea", Price = 2.99m};
+
+    //Act
+    testTea.Name = expectedName;
+    string actualName = testTea.Name;
+
+    //Assert
+    Assert.AreEqual(expectedName, testTea.Name);
+    }
   }
 }
